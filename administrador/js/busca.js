18ca8a1e7f9323cc2906ce08eb23f7f34df10403
Jair14 <hernandez_yo14@outlook.es>
2018-05-16 17:52:02 -0500

    Comentarios a js

--- administrador/js/busca.js
+++ administrador/js/busca.js
@@ -1,30 +1,30 @@
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+
+function getParameterByName(name) {// Función que nos permitirá obtener valores de la URL
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]"); // Se establece una expresión regular para lograr extraer valores de la URL
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    results = regex.exec(location.search); // Búscamos en la URL según la expresión regular
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")); // Devolvemos los resultados obtenidos
 }
-var folio = getParameterByName('folio');
-var ej = "a";
-$(buscar_datos(ej, folio));
-function buscar_datos(consulta, folio){
-    $.ajax({
-        url: 'buscarp.php',
-        type: 'POST',
-        dataType: 'html',
-        data: {consulta: consulta, folio: folio},
+var folio = getParameterByName('folio'); // Obtenemos el folio de la URK
+var ej = "a"; //Establecemos un valor inicial para comenzar la búsqueda
+$(buscar_datos(ej, folio)); // Cuando haya cargado la página, se ejecutará la funcion buscar_datos
+function buscar_datos(consulta, folio){ // Función buscar_datos
+    $.ajax({ // Se establece una estructura AJAX para la búsqueda en tiempo real
+        url: 'buscarp.php', // Establecemos la URL  a la que se dirigirá
+        type: 'POST', // Se enviará mediante método POST
+        dataType: 'html', // El tipo de dato es html
+        data: {consulta: consulta, folio: folio}, // Enviamos parametros a la funcion AJAX
     })
-    .done(function(respuesta){
-        
-        $("#datos").html(respuesta);
+    .done(function(respuesta){ // Función que se ejecutará al resultar exitosa la petición asíncrona
+        $("#datos").html(respuesta); // Le insertamos la respuesta del servidor al id datos
     })
-    .fail(function(){
-        console.log("Error");
+    .fail(function(){ // Función que se ejecutará al fallar la petición AJAX
+        console.log("Error"); // Se manda a consola el error
     })
 }
-$(document).on('keyup', '#buscar', function(){
-    var valor = $(this).val();
-    if (valor != "") {
+$(document).on('keyup', '#buscar', function(){ // Al presionar una tecla se ejecutará la función
+    var valor = $(this).val(); // Se obtiene el valor del id buscar
+    if (valor != "") { // Si el valor contiene algo, se ejecutará la funcion buscar_datos con los valores de folio y el valor obtenido
         buscar_datos(valor, folio);
     }else{
         buscar_datos(valor, folio);
--- administrador/js/resultados.js
+++ administrador/js/resultados.js
@@ -1,21 +1,21 @@
-$(buscar_datos());
-function buscar_datos(consulta){
-    $.ajax({
-        url: 'busquedaresultados.php',
-        type: 'POST',
-        dataType: 'html',
-        data: {consulta: consulta},
+$(buscar_datos()); // Cuando haya cargado la página, se ejecutará la funcion buscar_datos
+function buscar_datos(consulta){ // Función buscar_datos
+    $.ajax({ // Se establece una estructura AJAX para la búsqueda en tiempo real
+        url: 'busquedaresultados.php', // Establecemos la URL  a la que se dirigirá
+        type: 'POST', // Se enviará mediante método POST
+        dataType: 'html', // El tipo de dato es html
+        data: {consulta: consulta}, // Enviamos parametros a la funcion AJAX
     })
-    .done(function(respuesta){
-        $("#datos").html(respuesta);
+    .done(function(respuesta){ // Función que se ejecutará al resultar exitosa la petición asíncrona
+        $("#datos").html(respuesta); // Le insertamos la respuesta del servidor al id datos
     })
-    .fail(function(){
-        console.log("Error");
+    .fail(function(){  // Función que se ejecutará al fallar la petición AJAX
+        console.log("Error"); // Se manda a consola el error
     })
 }
-$(document).on('keyup', '#buscar', function(){
-    var valor = $(this).val();
-    if (valor != "") {
+$(document).on('keyup', '#buscar', function(){  // Al presionar una tecla se ejecutará la función
+    var valor = $(this).val();  // Se obtiene el valor del id buscar
+    if (valor != "") { // Si el valor contiene algo, se ejecutará la funcion buscar_datos con los valores de folio y el valor obtenido
         buscar_datos(valor);
     }else{
         buscar_datos();
