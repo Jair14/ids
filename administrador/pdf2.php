b65cb8299c5730c66b26448c775ea6ffab1903b2
Jair14 <hernandez_yo14@outlook.es>
2018-06-13 08:37:42 -0500

    Versión final

--- administrador/pdf2.php
+++ administrador/pdf2.php
@@ -14,35 +14,37 @@
   $pdf->SetX(40); // Mandamos una posicion en y
 	$pdf->Cell(120,0, 'Preguntas',0,0,'C');  // Imprimimos preguntas
 	$pdf->Ln(10);
-	$pdf->Cell(40,6,'Seccion',1,0,'C',1); // Imprimimos seccion
-	$pdf->Cell(75,6,'Pregunta',1,0,'C',1); // Imprimimos pregunta
-	$pdf->Cell(75,6,'Respuesta correcta',1,1,'C',1); // Imprimimos Respuesta_correcta
+	$pdf->Cell(30,6,'Seccion',1,0,'C',1); // Imprimimos seccion
+	$pdf->Cell(30,6,'Id de Pregunta',1,0,'C',1); // Imprimimos pregunta
+	$pdf->Cell(65,6,'Pregunta',1,0,'C',1); // Imprimimos pregunta
+	$pdf->Cell(65,6,'Respuesta correcta',1,1,'C',1); // Imprimimos Respuesta_correcta
 
 	$pdf->SetFont('Arial','',7);
 
 	while($row = $resultado->fetch_assoc()) // Arreglo asociativo
 	{
-		$pdf->Cell(40,6,utf8_decode($row['Materia']),1,0,'C'); // Imprimimos la materia
+		$pdf->Cell(30,6,utf8_decode($row['Materia']),1,0,'C'); // Imprimimos la materia
+		$pdf->Cell(30,6,utf8_decode($row['Id_pregunta']),1,0,'C'); // Imprimimos la materia
 		// Limitamos la respuesta dada
 		if(strlen($row['Pregunta'])>60){
 			$resp = "";
-			for($i = 0; $i <= 60; $i++ ){
+			for($i = 0; $i <= 50; $i++ ){
 				$resp .= $row['Pregunta'][$i];
 			}
 			$resp .= "...";
-			$pdf->Cell(75,6,utf8_decode($resp),1,0,'C');
+			$pdf->Cell(65,6,utf8_decode($resp),1,0,'C');
 		}else{
-			$pdf->Cell(75,6,utf8_decode($row['Pregunta']),1,0,'C');
+			$pdf->Cell(65,6,utf8_decode($row['Pregunta']),1,0,'C');
     }
     if(strlen($row['Respuesta_correcta'])>60){
 			$resp = "";
-			for($i = 0; $i <= 60; $i++ ){
+			for($i = 0; $i <= 50; $i++ ){
 				$resp .= $row['Respuesta_correcta'][$i];
 			}
 			$resp .= "...";
-			$pdf->Cell(75,6,utf8_decode($resp),1,1,'C');
+			$pdf->Cell(65,6,utf8_decode($resp),1,1,'C');
 		}else{
-			$pdf->Cell(75,6,utf8_decode($row['Respuesta_correcta']),1,1,'C');
+			$pdf->Cell(65,6,utf8_decode($row['Respuesta_correcta']),1,1,'C');
 		}
 	}
 
